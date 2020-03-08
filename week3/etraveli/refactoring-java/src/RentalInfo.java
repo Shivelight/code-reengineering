ef3ebbbcbd0d77c7bcab6b1e3c0f9dc775456cda
Shivelight <cwkfr@protonmail.com>
2020-03-08 23:15:47 +0700

    Refactor Movie to use MovieCode enum
    IMO using string is a code smell here.

--- week3/etraveli/refactoring-java/src/RentalInfo.java
+++ week3/etraveli/refactoring-java/src/RentalInfo.java
@@ -4,10 +4,10 @@ public class RentalInfo {
 
   public String statement(Customer customer) {
     HashMap<String, Movie> movies = new HashMap();
-    movies.put("F001", new Movie("You've Got Mail", "regular"));
-    movies.put("F002", new Movie("Matrix", "regular"));
-    movies.put("F003", new Movie("Cars", "childrens"));
-    movies.put("F004", new Movie("Fast & Furious X", "new"));
+    movies.put("F001", new Movie("You've Got Mail", MovieCode.REGULAR));
+    movies.put("F002", new Movie("Matrix", MovieCode.REGULAR));
+    movies.put("F003", new Movie("Cars", MovieCode.CHILDREN));
+    movies.put("F004", new Movie("Fast & Furious X", MovieCode.NEW));
 
     double totalAmount = 0;
     int frequentEnterPoints = 0;
@@ -16,16 +16,16 @@ public class RentalInfo {
       double thisAmount = 0;
 
       // determine amount for each movie
-      if (movies.get(r.getMovieId()).getCode().equals("regular")) {
+      if (movies.get(r.getMovieId()).getCode().equals(MovieCode.REGULAR)) {
         thisAmount = 2;
         if (r.getDays() > 2) {
           thisAmount = ((r.getDays() - 2) * 1.5) + thisAmount;
         }
       }
-      if (movies.get(r.getMovieId()).getCode().equals("new")) {
+      if (movies.get(r.getMovieId()).getCode().equals(MovieCode.NEW)) {
         thisAmount = r.getDays() * 3;
       }
-      if (movies.get(r.getMovieId()).getCode().equals("childrens")) {
+      if (movies.get(r.getMovieId()).getCode().equals(MovieCode.CHILDREN)) {
         thisAmount = 1.5;
         if (r.getDays() > 3) {
           thisAmount = ((r.getDays() - 3) * 1.5) + thisAmount;
@@ -35,7 +35,7 @@ public class RentalInfo {
       //add frequent bonus points
       frequentEnterPoints++;
       // add bonus for a two day new release rental
-      if (movies.get(r.getMovieId()).getCode() == "new" && r.getDays() > 2) frequentEnterPoints++;
+      if (movies.get(r.getMovieId()).getCode() == MovieCode.NEW && r.getDays() > 2) frequentEnterPoints++;
 
       //print figures for this rental
       result += "\t" + movies.get(r.getMovieId()).getTitle() + "\t" + thisAmount + "\n";
